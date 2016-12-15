d70b4704574584afdcff384ed6cbd5bc735bee41
Petr Kessler <kesspess@gmail.com>
2016-12-15 09:03:59 +0100

    removed pointless comments

--- src/SparseMatrix/SparseMatrix.h
+++ src/SparseMatrix/SparseMatrix.h
@@ -17,155 +17,58 @@
 	using namespace std;
 
 
-	template <typename T>
+	template<typename T>
 	class SparseMatrix
 	{
 
 		public:
 
-			/**
-			 * Creates a square matrix
-			 *
-			 * @param  n number of rows and columns
-			 */
-			SparseMatrix(int n);
+			// === CREATION ==============================================
 
+			SparseMatrix(int n); // square matrix n×n
+			SparseMatrix(int rows, int columns); // general matrix
 
-			/**
-			 * Creates a general matrix
-			 *
-			 * @param  rows number of rows
-			 * @param  columns number of columns
-			 */
-			SparseMatrix(int rows, int columns);
-
-
-			/**
-			 * Copy constructor
-			 *
-			 * @param  m instance to be copied
-			 */
-			SparseMatrix(const SparseMatrix<T> & m);
-
-
-			/**
-			 * Assignment operator
-			 *
-			 * @param  m matrix we're assigning
-			 * @return deep copy of assigned matrix
-			 */
+			SparseMatrix(const SparseMatrix<T> & m); // copy constructor
 			SparseMatrix<T> & operator = (const SparseMatrix<T> & m);
-
 
 			~SparseMatrix(void);
 
 
-			/**
-			 * Number of rows
-			 *
-			 * @return int
-			 */
+			// === GETTERS / SETTERS ==============================================
+
 			int getRowCount(void) const;
-
-
-			/**
-			 * Number of columns
-			 *
-			 * @return int
-			 */
 			int getColumnCount(void) const;
 
 
-			/**
-			 * Element getter
-			 *
-			 * @param  row
-			 * @param  col
-			 * @return element at given position or empty element if not set yet
-			 */
+			// === VALUES ==============================================
+
 			T get(int row, int col) const;
-
-
-			/**
-			 * Element setter
-			 *
-			 * @param  value
-			 * @param  row
-			 * @param  col
-			 * @return self for fluent interface
-			 */
 			SparseMatrix & set(T val, int row, int col);
 
 
-			/**
-			 * Multiplies matrix with vector
-			 *
-			 * @param  x vector
-			 * @return result of the product
-			 */
+			// === OPERATIONS ==============================================
+
 			vector<T> multiply(const vector<T> & x) const;
 			vector<T> operator * (const vector<T> & x) const;
 
-
-			/**
-			 * Multiplies matrix by another matrix
-			 *
-			 * @param  m
-			 * @return result of the product
-			 */
 			SparseMatrix<T> multiply(const SparseMatrix<T> & m) const;
 			SparseMatrix<T> operator * (const SparseMatrix<T> & m) const;
 
-
-			/**
-			 * Adds another matrix to this matrix
-			 *
-			 * @param  m
-			 * @return result of the sum
-			 */
 			SparseMatrix<T> add(const SparseMatrix<T> & m) const;
 			SparseMatrix<T> operator + (const SparseMatrix<T> & m) const;
 
-
-			/**
-			 * Subtracts another matrix from this matrix
-			 *
-			 * @param  m
-			 * @return result of the subraction
-			 */
 			SparseMatrix<T> subtract(const SparseMatrix<T> & m) const;
 			SparseMatrix<T> operator - (const SparseMatrix<T> & m) const;
 
 
-			/**
-			 * Compares two matrices
-			 *
-			 * @param  a
-			 * @param  b
-			 * @return are both matrices equal?
-			 */
+			// === FRIEND FUNCTIONS =========================================
+
 			template<typename X>
 			friend bool operator == (const SparseMatrix<X> & a, const SparseMatrix<X> & b);
 
-
-			/**
-			 * Compares two matrices
-			 *
-			 * @param  a
-			 * @param  b
-			 * @return are both matrices unequal?
-			 */
 			template<typename X>
 			friend bool operator != (const SparseMatrix<X> & a, const SparseMatrix<X> & b);
 
-
-			/**
-			 * Prints matrix in human friendly format
-			 *
-			 * @param  os output stream
-			 * @param  matrix to print
-			 * @return output stream for fluent interface
-			 */
 			template<typename X>
 			friend ostream & operator << (ostream & os, const SparseMatrix<X> & matrix);
 
@@ -178,56 +81,13 @@
 			vector<int> * rows, * cols;
 
 
-			/**
-			 * Common internal constructor
-			 *
-			 * @param  m number of rows
-			 * @param  n number of columns
-			 */
+			// === HELPERS / VALIDATORS ==============================================
+
 			void construct(int m, int n);
-
-
-			/**
-			 * Common internal destructor
-			 */
 			void destruct(void);
-
-
-			/**
-			 * Copies matrix (used in copy constructor and assignment operator)
-			 *
-			 * @param  m matrix to be copied
-			 * @return void
-			 */
 			void deepCopy(const SparseMatrix<T> & m);
-
-
-			/**
-			 * Element coordinate validator
-			 *
-			 * @param  row
-			 * @param  col
-			 */
 			void validateCoordinates(int row, int col) const;
-
-
-			/**
-			 * Inserts given value into the matrix
-			 *
-			 * @param  index at which the new element will be placed
-			 * @param  row
-			 * @param  col
-			 * @param  val
-			 */
 			void insert(int index, int row, int col, T val);
-
-
-			/**
-			 * Removes value from the matrix
-			 *
-			 * @param  index
-			 * @param  row
-			 */
 			void remove(int index, int row);
 
 	};
